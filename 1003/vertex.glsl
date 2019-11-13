c1f1dc7d6ccc3aea8de01533da2115e607eabee0
YSH <2016180054@kpu.ac.kr>
2019-11-13 22:03:50 +0900

    3차원도형그리기 (구,원뿔,디스크,실린더)

--- 1003/vertex.glsl
+++ 1003/vertex.glsl
@@ -7,8 +7,8 @@
 in  vec3 in_Position; // 위치속성 
 in  vec3 in_Color; // 색상속성
 out vec3 ex_Color; // 프래그먼트세이더에게전달
-
+uniform mat4 u_transform;
 void main(void) { 
-	gl_Position = vec4 (in_Position.x, in_Position.y, in_Position.z, 1.0);
-	ex_Color = in_Color;
+	gl_Position = u_transform*vec4 (in_Position.x, in_Position.y, in_Position.z, 1.0);
+	ex_Color = vec3(1,1,1);
 }
